4724d114c519317ed27b6debc9ac122a43686dc9
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-20 17:44:41 +0300

    resolve task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая на вход принимает трёхначное число и на выходе показывает вторую цифру этого чила.
+int randomNum = new Random().Next(100, 1000);
+Console.WriteLine(randomNum);
+int num = (randomNum % 100 - randomNum % 10) / 10;
+Console.WriteLine(num);

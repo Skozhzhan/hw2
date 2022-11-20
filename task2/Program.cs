19822a6851b402d2ce72330e48703d4fa00856c1
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-20 20:28:26 +0300

    final

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,31 +1,31 @@
 ﻿// Напишите программу, котораяя выводит третью цифру заданного числа или сообщает, что третьей цифры нет
-// Вариант решения с делением - не разобралась до конца, но мне кажется направление верное
-// Console.WriteLine("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int k = Convert.ToInt32(Console.ReadLine());
-// if (num <= 99)
-// {
-//     Console.WriteLine($"Третьей цифры нет");
-// }
-// else
-// {
-//     while (num >= 100)
-//     {
-//         k = num / 10;
-//     }
-//     int result = k % 10;
-//     Console.WriteLine(result);
-// }
+// Вариант решения строкой
+//Console.WriteLine("Введите любое число: ");
+//string num = Console.ReadLine();
+//if (num.Length >= 3)
+//{
+//    Console.WriteLine($"Третья цифра введенного числа: {num[2]}");
+//}
+//else
+//{
+//    Console.WriteLine("Третьей цифры нет");
+//}
 
-Console.WriteLine("Введите любое число: ");
-string num = Console.ReadLine();
 
-if (num.Length >= 3)
-
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 99)
 {
-    Console.WriteLine($"Третья цифра введенного числа: {num[2]}");
+    Console.WriteLine($"Третьей цифры нет");
 }
 else
 {
-    Console.WriteLine("Третьей цифры нет");
+    while (num > 999)
+
+    {
+        num /= 10;
+    }
+    int result = num % 10;
+    Console.WriteLine(result);
 }
+
